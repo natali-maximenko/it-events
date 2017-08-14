16e290f5a6cf3ebf6fd86840f3fefc05ddb6efda
Natali Maximenko <natali.maximenko@gmail.com>
2017-08-15 00:12:38 +0300

    fix production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
+      region:                ENV['AWS_REGION'],            # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME']  # required
+    config.fog_public     = true                   # optional, defaults to true
+    config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional
+  enabled
 end
